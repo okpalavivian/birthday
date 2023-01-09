24ffaeb96e0603dbcc287c5c3a68c99b0258e306
Viva Dev <109100020+okpalavivian@users.noreply.github.com>
2023-01-08 19:30:00 -0800

    update

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./hero.css"
+const Hero = () => {
+  return (
+    <div className="hero">
+    <div className='left'>left</div>
+    <div className='center'>center</div>
+    <div className='right'>
+        <img src="/img/pix.png" alt=''/>
+</div>
+    </div>
+  )
+}
+
+export default Hero
