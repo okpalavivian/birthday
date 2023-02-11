1f087517a40f3c31e79bded245e212dc3cb9b19e
okpalavivian <vu.okpala@unizik.edu.ng>
2023-02-11 19:05:20 +0100

    update

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./hero.css"
+import Pix from "../../../src/img/pix.png"
 const Hero = () => {
   return (
 <>
@@ -7,7 +8,7 @@ const Hero = () => {
     <div className='left'>left</div>
     <div className='center'>center</div>
     <div className='right'>
-        <img src="/img/pix.png" alt=''/>
+        <img src= {Pix} alt=''/>
 </div>
 <div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,288L80,282.7C160,277,320,267,480,224C640,181,800,107,960,112C1120,117,1280,203,1360,245.3L1440,288L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg></div>
     </div></>
